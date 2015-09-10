ed999ddc3ca16eb9095361a5cb8884314b9ea140
mmantena <mmantena@quadratics.com>
2015-09-10 14:40:25 +0530

    mmmm

--- BigData/src/main/java/com/bigdata/analytics/bigdata/App.java
+++ BigData/src/main/java/com/bigdata/analytics/bigdata/App.java
@@ -49,7 +49,6 @@ public class App
 			});
 			System.out.println("MR Job success");
 			System.out.println("MR Job completed successfully");
-			System.out.println("From different");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
